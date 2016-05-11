d5fb57b7eb6343cbc2f40ba7ec0f812ee5414b04
ysw <yinshaowen241@gmail.com>
2016-05-11 14:42:01 +0800

    implement mpris2 plugin

---
+++ __init__.py
@@ -0,0 +1,25 @@
+import logging
+
+from feeluown.utils import is_linux
+
+
+__alias__ = 'linux mpris2'
+__version__ = '0.0.1'
+__desc__ = '实现 linux 的 mpris2 dbus 接口'
+
+
+def enable(app):
+    logger = logging.getLogger(__name__)
+    if not is_linux():
+        logger.warning('not a linux system, wont load mpris2 plugin')
+        return
+    import dbus.mainloop.pyqt5
+    from .service import MprisServer
+    dbus.mainloop.pyqt5.DBusQtMainLoop(set_as_default=True)
+    MprisServer(app)
+    logger.info('load mpris2 plugin')
+
+
+def disable(app):
+    logger = logging.getLogger(__name__)
+    logger.info('disable mpris2 plugin')
