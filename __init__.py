fde8a38e5ad734e78dc01a2bc4b52708f0b7afd7
cosven <yinshaowen241@gmail.com>
2019-03-22 19:57:50 +0800

    compat with feeluown 3.0

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
+import platform
 import logging
-
-from feeluown.utils import is_linux
 
 
 __alias__ = 'linux mpris2'
@@ -10,7 +9,7 @@ __desc__ = '实现 linux 的 mpris2 dbus 接口'
 
 def enable(app):
     logger = logging.getLogger(__name__)
-    if not is_linux():
+    if platform.system() != 'Linux':
         logger.warning('not a linux system, wont load mpris2 plugin')
         return
     import dbus.mainloop.pyqt5
